7b7595e9521e66574193f6a4eac712889cbcb750
William Sifleet <WSifleet@GMail.com>
2020-05-07 12:42:54 -0400

    Version 1

---
+++ myNewLibrary/myFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+
+namespace myNewLibrary
+{
+    public static class myFunctions
+    {
+        [ExcelFunction(Description = "My first .NET function")]
+        public static string SayHello(string name)
+        {
+            return "Hello v1 " + name;
+        }
+
+    }
+
+}
